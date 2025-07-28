9edaf2c3e3d36f35293aa79d8359b3491fd6a627
GerAle30 <garciaslj@gmail.com>
2025-07-28 17:47:54 -0400

    Fix formatting and improve docstring in measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""MOdule to measure the runtime of wait_n using asyncio and time."""
+"""Module to measure the runtime of wait_n using asyncio and time."""
 
-import time import aasyncio
+import time
+import asyncio
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measure total runtime of wait_n(n, max_delay),
-    and return average execution time per coroutine"""
-
+    """Measures the total runtime of wait_n(n, max_delay)
+    and returns the average time per coroutine.
+    """
     start_time = time.perf_counter()
-    asyncio.run(wait_n(n, mas_delay))
-    end_time = end_time - start_time
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
     return total_time / n
+
