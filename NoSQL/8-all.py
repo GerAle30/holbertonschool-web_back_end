ec90ebfa96830304cb27a15bc222658c56bd3c15
GerAle30 <garciaslj@gmail.com>
2025-08-15 18:16:23 -0400

    update

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,16 +3,15 @@
 Module that provides a function to list all document in a collection
 """
 
+
 def list_all(mongo_collection):
     """
     List all document in a collection
     Args:
-        mongo_collection: pymongo collection object 
+        mongo_collection: pymongo collection object
 
-        Returns:
-            List of document in the colleciton, empty list if no documents
-            """
-            
-            document = list(mongo_collection.find())
-            return documents
-
+    Returns:
+        List of document in the colleciton, empty list if no documents
+    """
+    documents = list(mongo_collection.find())
+    return documents
