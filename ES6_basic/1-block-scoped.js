a1e37688e78b6e69cf3add6888a1a94f620fb388
GerAle30 <garciaslj@gmail.com>
2025-08-19 15:11:00 -0400

    fix the block-scoped

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,14 +1,15 @@
-// 1-block-scoped.js
-
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
+  if (trueOrFalse) {
+    // These are block-scoped and will not overwrite the outer bindings
+    const task = true;
+    const task2 = false;
+    void task; // avoid linter complaints about unused variables
+    void task2;
+  }
 
-    }
-
-    return [task, task2];
+  return [task, task2];
 }
+
