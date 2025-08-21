8009e43df467764ed505a34e8c3169b31149f458
GerAle30 <garciaslj@gmail.com>
2025-08-21 16:11:10 -0400

    fix the return statement: and fix the name ClassRoom

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,10 +2,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-    return
-    [
-        new Classroom(19),
-        new Classroom(20),
-        new Classroom(34),
+    return [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34),
     ];
 }
