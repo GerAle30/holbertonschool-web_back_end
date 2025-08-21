5ecb951f908ab4f662d28dcb7687a25a616adffc
GerAle30 <garciaslj@gmail.com>
2025-08-21 14:23:44 -0400

    Function executes properly

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,12 @@
 // 7-getBudgetObject.js
 
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
   return budget;
 }
+
