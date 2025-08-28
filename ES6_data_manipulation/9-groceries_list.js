6c51430f7a76165226202c0f3a3157ff633801f7
GerAle30 <garciaslj@gmail.com>
2025-08-28 19:17:32 -0400

    create a groceries list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+    const groceries = new Map();
+    groceries.set('Apple', 10);
+    groceries.set('tomatoes', 10);
+    groceries.set('Pasta', 1);
+    groceries.set('Rice', 1);
+    groceries.set('Banana', 5);
+
+    return groceries;
+}
