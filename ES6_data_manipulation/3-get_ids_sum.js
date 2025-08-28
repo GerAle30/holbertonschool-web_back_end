9888cf3ec4b5dceab0d1c3351e43a0e8e85d6488
GerAle30 <garciaslj@gmail.com>
2025-08-28 15:16:02 -0400

    fix error in the sum exercise

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,20 +1,2 @@
-function getstudentsIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return 0;
-  }
-  return students.reduce((sum, student) => sum + student.id, 0);
-}
-
-export default getstudentsIdsSum;
-
 export default function getStudentsIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return 0;
-  }
-  return students.reduce((sum, student) => sum + student.id, 0);
-}
-
-
-
-
-
+    return students.reduce((acc. students) => acc + students.id, 0);
