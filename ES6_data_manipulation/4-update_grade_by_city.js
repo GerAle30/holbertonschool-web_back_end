6357b49a1f0f220b6db58a66f9cd0b8f8776d390
GerAle30 <garciaslj@gmail.com>
2025-08-28 15:59:58 -0400

    implement update of students like city,new grade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentsGradeByCity(students, city, newGrades) {
     return students
     .filter((students) => students.location === city)
     .map((student) => {
-        const gradeObj = newGrades.find((g) => g.studetnId === student.id);
+        const gradeObj = newGrades.find((g) => g.studentId === student.id);
         return {
             ...student,
             grade: gradeObj ? gradeObj.grade : 'N/A',
