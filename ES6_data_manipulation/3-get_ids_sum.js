4c712427d7e0fa9f157970596733821ab1080fae
GerAle30 <garciaslj@gmail.com>
2025-08-28 15:27:18 -0400

    Fixed Syntax error change acc, students

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
     if (!Array.isArray(students)) return 0;
-    return students.reduce((acc, students) => acc + student.id, 0);
+    return students.reduce((acc, student) => acc + student.id, 0);
 }
--- ES6_data_manipulation/3-main.js
+++ ES6_data_manipulation/3-main.js
@@ -2,6 +2,6 @@ import getListStudents from "./0-get_list_students.js";
 import getStudentsIdsSum from "./3-get_ids_sum.js";
 
 const students = getListStudents();
-const value = getstudentsIdsSum(students);
+const value = getStudentsIdsSum(students);
 
 console.log(value);
