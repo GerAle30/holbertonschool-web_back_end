6f0eae46c102f92e732e53201bf6b532a739a7d2
GerAle30 <garciaslj@gmail.com>
2025-08-29 18:07:53 -0400

    fix guardrail

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,12 +1,15 @@
+
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        const value = mathFunction();
-        queue.push(value);
-    }catch (err) {
-        queue.push('Error: ${err.message}');
-    } finally {
-        queue.push('Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (err) {
+    // <-- usa template literal correcto:
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
+
