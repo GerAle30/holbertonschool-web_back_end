6c77fd65d7b2076da121e3adc19fe887884ac192
GerAle30 <garciaslj@gmail.com>
2025-09-02 17:53:31 -0400

    fix error with fil;e

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,9 +7,9 @@ process.stdin.on('data', (data) => {
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
-  process.stdin.end();
+  process.stdin.exit();
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
