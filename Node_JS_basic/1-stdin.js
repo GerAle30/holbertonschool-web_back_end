08e7cd165f3c7607dcebe5cfec3ec48bf94055f8
GerAle30 <garciaslj@gmail.com>
2025-09-02 17:41:57 -0400

    I use different line of code and check the diferent use if loops to work

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+  process.stdin.end();
 });
 
-process.on('exit', () => {
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
 
