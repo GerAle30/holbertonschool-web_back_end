8ad71f1f72c44b90522c49e1292a154cd14cd9f4
GerAle30 <garciaslj@gmail.com>
2025-09-02 18:03:39 -0400

    I use node to see the problem in stdin And I fixed now check run the chekcer correctly

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,16 +2,19 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    const name = chunk.toString().trim();
-    if (name) {
-      process.stdout.write(`Your name is: ${name}\n`);
-    }
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+  if (process.stdin.isTTY) {
+    process.exit();
   }
 });
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
