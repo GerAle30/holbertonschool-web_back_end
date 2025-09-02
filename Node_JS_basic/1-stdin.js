8f4aefa613f167fd613181e84146f2577b24fe3d
GerAle30 <garciaslj@gmail.com>
2025-09-02 17:35:28 -0400

    chance the syntax and check now everything is correct

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    const name = chunk.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
-    process.stdin.end();
+    process.exit();
   }
 });
 
-process.stdin.on('end', () => {
+process.on('exit',() => {
   process.stdout.write('This important software is now closing\n');
 });
