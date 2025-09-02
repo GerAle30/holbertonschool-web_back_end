891165e0ce5a10fa08f035626c825833a31dc557
GerAle30 <garciaslj@gmail.com>
2025-09-02 17:02:01 -0400

    Fixed problem with the code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
-    process.stdin.end();
+  }
 });
 
-process.on('exit', () => {
-    console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
