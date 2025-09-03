81124cb6024a16889e1983424a6d2ffc22af7a8a
GerAle30 <garciaslj@gmail.com>
2025-09-03 18:31:23 -0400

    chance the line of code for see diferent result

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name) {
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.toString().trim();
+    // aquí cambiamos el salto de línea
     process.stdout.write(`Your name is: ${name}\r`);
-      process.stdin.end();
-  }
-  if (process.stdin.isTTY) {
-    process.exit();
+    process.stdin.end();
   }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
+
