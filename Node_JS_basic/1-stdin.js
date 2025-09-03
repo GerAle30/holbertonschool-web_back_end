0aa02dd20b7f96f48d919851e2123e594e3fceb8
GerAle30 <garciaslj@gmail.com>
2025-09-03 19:34:12 -0400

    Check and fix the error with syntax

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (chunk) => {
   const name = chunk.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}\n`);
   process.stdin.pause();
 });
 
