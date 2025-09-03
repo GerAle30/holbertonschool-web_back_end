18b5529b16b5028f68a4320020894634b6e93752
GerAle30 <garciaslj@gmail.com>
2025-09-03 18:56:31 -0400

    I use node to see the problem in stdin And I fixed now check run the checker correctly

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (chunk) => {
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     const name = chunk.toString().trim();
     process.stdout.write(`Your name is: ${name}\r`);
-    process.stdin.end();
   }
 });
 
