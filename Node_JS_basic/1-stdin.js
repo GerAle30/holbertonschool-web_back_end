937841dec3166194008c00b459d6887c8c12c744
GerAle30 <garciaslj@gmail.com>
2025-09-04 18:02:05 -0400

    I did the same chance but now correct I think

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}\n`);
   process.stdin.pause();
 });
 
